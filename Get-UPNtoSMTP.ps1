ee52ea15ac8b1dcb1847460e26ac5a4a459d283a
Pheo <ryan.coates@gmail.com>
2016-03-31 21:48:57 -0600

    Added Get-UPNtoSMTP.ps1

---
+++ Get-UPNtoSMTP.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-UPNtoSMTP
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([PSObject])]
+    Param
+    (
+        [Parameter(
+            Mandatory=$false
+        )]
+        [string]$ResultSize = 'Unlimited'
+    )
+
+    Begin
+    {
+       $OutputCollection = @()
+       $Users = get-eouser -resultsize $ResultSize -filter *
+       $Recip = get-eorecipient -RecipientTypeDetails UserMailbox -resultsize $ResultSize -filter * 
+    }
+    Process
+    {
+        $Recip | ForEach-Object{
+        
+        $recipientObject = $_
+        $userObject = get-eouser $recipientObject.Identity
+    
+#        New-Object -TypeName PSObject -Property @{
+#            FirstName = $user.firstname
+#            LastName = $user.lastname
+#            UPN = $user.userprincipalname
+#            PrimarySMTPAddress = $recipient.primarysmtpaddress
+#            }
+        $outputObject = "" | Select FirstName, LastName, PrimarySMTPAddress, UPN
+        $outputObject.FirstName = $userObject.FirstName
+        $outputObject.LastName  = $userObject.LastName
+        $outputObject.PrimarySMTPAddress = $recipientObject.primarysmtpaddress
+        $outputObject.UPN = $userObject.userprincipalname
+        
+        $OutputCollection += $outputObject
+        }
+    }
+    End
+    {
+        Return $OutputCollection
+    }
+}
