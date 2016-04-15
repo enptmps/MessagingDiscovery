ff681ed4dbd9dac668c0f5c6b317e338e5169b1a
mmantooth3 <mmantooth3@gmail.com>
2016-04-15 11:05:16 -0600

    upload

---
+++ Get-RetentionPolicy.ps1
@@ -0,0 +1,5 @@
+Get-RetentionPolicy | Foreach-Object{
+$Policy = $_
+(Get-RetentionPolicy $Policy).RetentionPolicyTagLinks | `
+select @{n=”Policy”;e ={$Policy }},name
+} | export-Csv  C:\scripts\RetentionPolicyTagLinks.csv -nti 
