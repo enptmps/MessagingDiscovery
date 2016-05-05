30adab09f3ccd2a3176b956abfda2175fda2331a
mmantooth3 <mmantooth3@gmail.com>
2016-05-05 14:21:51 -0600

    Public Folder 2013 discovery

---
+++ PF2013Discovery.ps1
@@ -0,0 +1,10 @@
+[run on-premises in EMS - Exchange 2013]
+AccessRights
+Get-PublicFolder -Recurse | Get-PublicFolderClientPermission | select FolderName,User,@{Name='Access Rights';Expression={[string]::join(', ', $_.AccessRights)}} | Export-Csv PF2013permissionsACCESSRIGHTS.csv -NTI
+
+SendonBehalf
+Get-MAILPublicFolder -Recurse | Get-PublicFolderClientPermission | select FolderName,User,@{Name='GrantSendOnBehalfTo';Expression={[string]::join(', ', $_.GrantSendOnBehalfTo)}} | Export-Csv PF2013permissionsSENDONBEHALF.csv -NTI
+
+SendAs
+Get-MailPublicFolder | Get-ADPermission |  ? { ($_.ExtendedRights-like "Send-as*") -or ($_.AccessRights  -eq "GenericAll") -and ($_.User -Notlike "NT AUTHORITY\SYSTEM") }| select identity, user, AccessRights, ExtendedRights | Export-Csv PF2013permissionsSENDAS.csv -NTI
+
