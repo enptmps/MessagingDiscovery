0084367f347ddaf504481ce98bc18070df4eb720
mmantooth3 <mmantooth3@gmail.com>
2016-07-15 15:24:13 -0600

    included more permissions

--- MbxPermissions.ps1
+++ MbxPermissions.ps1
@@ -1,10 +1,12 @@
-$OutFile = "C:\Temp\PermissionExport.txt"
-"DisplayName" + "," + "Alias" + "," + "Full Access" + "," + "Send As" | Out-File $OutFile -Force
- 
-$Mailboxes = Get-Mailbox -ResultSize:Unlimited | Select Identity, Alias, DisplayName, DistinguishedName
-ForEach ($Mailbox in $Mailboxes) {
-              $SendAs = Get-ADPermission $Mailbox.DistinguishedName | ? {$_.ExtendedRights -like "Send-As" -and $_.User -notlike "NT AUTHORITY\SELF" -and !$_.IsInherited} | % {$_.User}
-              $FullAccess = Get-MailboxPermission $Mailbox.Identity | ? {$_.AccessRights -eq "FullAccess" -and !$_.IsInherited} | % {$_.User}
- 
-              $Mailbox.DisplayName + "," + $Mailbox.Alias + "," + $FullAccess + "," + $SendAs | Out-File $OutFile -Append
-}
+$OutFile = "C:\Temp\PermissionExport.txt" 
+"DisplayName" + "," + "Alias" + "," + "Primary SMTP" + "," + "Full Access" + "," + "Send As" + "," + "Send on Behalf" | Out-File $OutFile -Force 
+
+$Mailboxes = Get-Mailbox -ResultSize:Unlimited | Select Identity, Alias, DisplayName, DistinguishedName, primarysmtpaddress 
+ForEach ($Mailbox in $Mailboxes) 
+{ 
+       $SendAs = Get-ADPermission $Mailbox.DistinguishedName | ? {$_.ExtendedRights -like "Send-As" -and $_.User -notlike "NT AUTHORITY\SELF" -and !$_.IsInherited} | % {$_.User} 
+       $FullAccess = Get-MailboxPermission $Mailbox.Identity | ? {$_.AccessRights -eq "FullAccess" -and !$_.IsInherited} | % {$_.User} 
+       $sendbehalf=Get-Mailbox $Mailbox.Identity | select-object -expand grantsendonbehalfto | select-object -expand rdn | % {$_.User} 
+       if (!$SendAs -and !$FullAccess -and !$sendbehalf){continue}
+       $Mailbox.DisplayName + "," + $Mailbox.Alias + "," + $Mailbox.primarysmtpaddress + "," + $FullAccess + "," + $SendAs + "," + $sendbehalf | Out-File $OutFile -Append 
+ }  
