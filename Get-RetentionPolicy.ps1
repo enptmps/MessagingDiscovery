0408c7d29d6e393c0ef8865090c3662e35426571
Pheo <ryan.coates@gmail.com>
2016-09-11 18:19:23 -0600

    Updates to fix issue with retention tag
    Updates to fix issue raised by @adminicrator
    https://github.com/enptmps/MessagingDiscovery/issues/10

--- Get-RetentionPolicy.ps1
+++ Get-RetentionPolicy.ps1
@@ -1,5 +1,11 @@
-Get-RetentionPolicy | Foreach-Object{
-$Policy = $_
-(Get-RetentionPolicy $Policy).RetentionPolicyTagLinks | `
-select @{n="Policy";e ={$Policy }},name
-} | export-Csv  C:\scripts\RetentionPolicyTagLinks.csv -nti 
+Set-ADServersettings -ViewEntireForest:$true
+
+$Policies = Get-RetentionPolicy
+
+$Output =  ForEach($Policy In $Policies) { $Policy.RetentionPolicyTagLinks | Get-RetentionPolicyTag | Select @{Name = "Policy";e = {$Policy.Name }},
+Name,`
+#@{Name = "AgeLimit"; Expression = { $.AgeLimitForRetention.ToString() }}
+@{Name = "AgeLimit"; Expression = {[Int]$.AgeLimitForRetention.ToString().Split(".")[0]}}
+}
+
+$Output | Export-Csv "C:\scripts\RetentionPolicyTagLinks.csv" -nti
